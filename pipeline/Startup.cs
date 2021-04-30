3b37822ace7a4f32a36c8395646f1f551565a4de
ayoadetunji <69466975+ayoadetunji@users.noreply.github.com>
2021-04-30 09:09:30 +0100

    GitHub Code Updated

--- pipeline/Startup.cs
+++ pipeline/Startup.cs
@@ -23,6 +23,7 @@ namespace pipeline
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //change1
             services.AddRazorPages();
         }
 
